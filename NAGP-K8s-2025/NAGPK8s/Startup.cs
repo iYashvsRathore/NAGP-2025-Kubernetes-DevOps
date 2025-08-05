2d89d1389576bf5913cad43cf6d35cb1cde7701a
Yash Rathore <yrathore007.yr@gmail.com>
2025-08-06 00:59:41 +0530

    Added User API

---
+++ NAGP-K8s-2025/NAGPK8s/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+using NAGPK8s.Repository;
+using NAGPK8s.Utilities;
+
+namespace NAGPK8s
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var dbConnectionString = DataUtil.ResolveDbConnectionString(Configuration);
+
+			services.AddControllers();
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+			services.AddDbContext<UserDBContect>(options => options.UseSqlServer(dbConnectionString));
+			services.AddTransient<UserRepository>();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.All
+			});
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
+
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "User API V1");
+			});
+		}
+	}
+}
